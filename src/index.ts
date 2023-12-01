c3e27fadb1a6756ae8602eb6581f6d944d3534c5
Leonardo Ferreira <leobarros_@outlook.com>
2023-12-01 16:41:43 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,95 @@
+let count = 0
+
+let addTaskBtn = document.getElementById('newTaskBtn') as HTMLButtonElement
+let addTaskInput = document.getElementById('newTaskInput') as HTMLInputElement
+
+addTaskBtn.addEventListener('click', ()=>{
+
+    if(addTaskInput.value.length === 0){
+        alert('Por favor, insira alguma atividade válida!')
+        return
+    }
+
+    count++
+    let taskArea = document.getElementById('tasks-area') as HTMLElement
+    let inputAreaText = document.getElementById('newTaskInput') as HTMLInputElement
+
+
+    let div = document.createElement('div')
+    div.id = `div-task${count}`
+    div.classList.add('task')
+    let input = document.createElement('input')
+    input.type = 'checkbox'
+    input.name = `task${count}`
+    input.id = `task${count}`
+    
+    
+    let deleteBtn = document.createElement('button')
+    deleteBtn.innerHTML = `<i class="fa-solid fa-trash-can"></i>`
+    deleteBtn.id = 'deleteBtn'
+
+    let label = document.createElement('label')
+    label.setAttribute('for', `${input.id}`)
+    label.innerText = inputAreaText.value
+
+    div.append(input, label)
+    taskArea.append(div, deleteBtn)
+
+    addTaskInput.value = ''
+
+    deleteBtn.addEventListener('click', ()=>{
+        const elementToDelete = deleteBtn.previousElementSibling
+        elementToDelete?.remove()
+        deleteBtn.remove()
+        count--
+    })
+})
+
+
+
+addTaskInput.addEventListener('keyup', (event)=>{
+
+    if(event.key === 'Enter'){
+
+        if(addTaskInput.value.length === 0){
+            alert('Por favor, insira alguma atividade válida!')
+            return
+        }else{
+            count++
+            let taskArea = document.getElementById('tasks-area') as HTMLElement
+            let inputAreaText = document.getElementById('newTaskInput') as HTMLInputElement
+        
+        
+            let div = document.createElement('div')
+            div.id = `div-task${count}`
+            div.classList.add('task')
+            let input = document.createElement('input')
+            input.type = 'checkbox'
+            input.name = `task${count}`
+            input.id = `task${count}`
+            
+            
+            
+            let label = document.createElement('label')
+            label.setAttribute('for', `${input.id}`)
+            label.innerText = inputAreaText.value
+
+
+            let deleteBtn = document.createElement('button')
+            deleteBtn.innerHTML = `<i class="fa-solid fa-trash-can"></i>`
+            deleteBtn.id = 'deleteBtn'
+
+
+            deleteBtn.addEventListener('click', ()=>{
+                const elementToDelete = deleteBtn.previousElementSibling
+                elementToDelete?.remove()
+                deleteBtn.remove()
+                count--
+            })
+        
+            div.append(input, label)
+            taskArea.append(div, deleteBtn)
+            addTaskInput.value = ''
+        }
+        } 
+})
